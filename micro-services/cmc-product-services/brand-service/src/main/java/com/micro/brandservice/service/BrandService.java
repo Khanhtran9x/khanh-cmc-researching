ad87ffedd9d847a358bc0826e520a3f7b1555cc6
khanhtran9x <khanh150795@gmail.com>
2022-10-19 18:26:45 +0700

    up

--- micro-services/cmc-product-services/brand-service/src/main/java/com/micro/brandservice/service/BrandService.java
+++ micro-services/cmc-product-services/brand-service/src/main/java/com/micro/brandservice/service/BrandService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@EnableEurekaClient
 public class BrandService {
     @Autowired
     private BrandRepository brandRepository;
--- micro-services/cmc-product-services/product-service/src/main/java/com/micro/productservice/constants/KafkaConstants.java
+++
@@ -1,7 +0,0 @@
-package com.micro.productservice.constants;
-
-public class KafkaConstants {
-    public static final String KAFKA_TOPIC = "kafka-product";
-    public static final String GROUP_ID = "product-sandbox";
-    public static final String KAFKA_BROKER = "localhost:9092";
-}
